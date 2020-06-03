8b2b6644b95bb9f45079e4e18f2320724f411ae9
sonny6310 <juny0413@naver.com>
2020-06-03 12:01:29 +0900

    8

---
+++ 20200603/ex01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01
+{
+    class User
+    {
+        public static double PI = 3.141592;
+        public DateTime regDate;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.regDate = DateTime.Now;
+            Console.WriteLine(user.regDate); // 인스턴스변수
+            Console.WriteLine($"PI = {User.PI}"); // 클래스변수
+            
+        }
+    }
+}
---
+++ 20200603/ex02/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex02
+{
+    class SecretMap
+    {
+        public SecretMap(int n, int[] arr1, int[] arr2)
+        {
+            string[] mapArr = new string[n];
+            string[] map = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                mapArr[i] = Convert.ToString((arr1[i] | arr2[i]), 2);
+                string input = "";
+                for (int j = 0; j < mapArr[i].Length; j++)
+                {
+
+                    if ((mapArr[i])[j] == '1')
+                    {
+                        input += "#";
+                    }
+                    else
+                    {
+                        input += " ";
+                    }
+                    map[i] = input;
+                }
+            }
+            Console.WriteLine("-------");
+            Console.WriteLine("비밀지도");
+            Console.WriteLine("-------");
+            foreach (var item in map)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------");
+        }
+    }
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 5;
+            int[] arr1_1 = { 9, 20, 28, 18, 11 };
+            int[] arr1_2 = { 30, 1, 21, 17, 28 };
+            SecretMap sm1 = new SecretMap(n1,arr1_1,arr1_2);
+
+            Console.WriteLine();
+
+            int n2 = 6;
+            int[] arr2_1 = { 46, 33, 33, 22, 31, 50 };
+            int[] arr2_2 = { 27, 56, 19, 14, 14, 10 };
+            SecretMap sm2 = new SecretMap(n2, arr2_1, arr2_2);
+        }
+    }
+}
