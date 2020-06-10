35c7d5ebe90b8a4c02a1e837754c14d17178a0b2
sonny6310 <juny0413@naver.com>
2020-06-10 16:16:18 +0900

    11

---
+++ 20200610/ex01/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ex01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.combo_Font = new System.Windows.Forms.ComboBox();
+            this.check_Bold = new System.Windows.Forms.CheckBox();
+            this.check_Italic = new System.Windows.Forms.CheckBox();
+            this.textBox_FontTest = new System.Windows.Forms.TextBox();
+            this.trackBar_Dummy = new System.Windows.Forms.TrackBar();
+            this.progressBar_Dummy = new System.Windows.Forms.ProgressBar();
+            this.button_Modal = new System.Windows.Forms.Button();
+            this.button_Modaless = new System.Windows.Forms.Button();
+            this.button_MessageBox = new System.Windows.Forms.Button();
+            this.treeView_Dummy = new System.Windows.Forms.TreeView();
+            this.listView_Dummy = new System.Windows.Forms.ListView();
+            this.button_AddRoot = new System.Windows.Forms.Button();
+            this.button_AddChild = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Dummy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "폰트: ";
+            // 
+            // combo_Font
+            // 
+            this.combo_Font.FormattingEnabled = true;
+            this.combo_Font.Location = new System.Drawing.Point(53, 6);
+            this.combo_Font.Name = "combo_Font";
+            this.combo_Font.Size = new System.Drawing.Size(122, 20);
+            this.combo_Font.TabIndex = 1;
+            this.combo_Font.SelectedIndexChanged += new System.EventHandler(this.onFontSelect);
+            // 
+            // check_Bold
+            // 
+            this.check_Bold.AutoSize = true;
+            this.check_Bold.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.check_Bold.Location = new System.Drawing.Point(181, 8);
+            this.check_Bold.Name = "check_Bold";
+            this.check_Bold.Size = new System.Drawing.Size(50, 16);
+            this.check_Bold.TabIndex = 2;
+            this.check_Bold.Text = "굵게";
+            this.check_Bold.UseVisualStyleBackColor = true;
+            this.check_Bold.CheckedChanged += new System.EventHandler(this.onBold);
+            // 
+            // check_Italic
+            // 
+            this.check_Italic.AutoSize = true;
+            this.check_Italic.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.check_Italic.Location = new System.Drawing.Point(235, 8);
+            this.check_Italic.Name = "check_Italic";
+            this.check_Italic.Size = new System.Drawing.Size(60, 16);
+            this.check_Italic.TabIndex = 3;
+            this.check_Italic.Text = "기울임";
+            this.check_Italic.UseVisualStyleBackColor = true;
+            this.check_Italic.CheckedChanged += new System.EventHandler(this.onItalic);
+            // 
+            // textBox_FontTest
+            // 
+            this.textBox_FontTest.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox_FontTest.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox_FontTest.Location = new System.Drawing.Point(12, 46);
+            this.textBox_FontTest.Name = "textBox_FontTest";
+            this.textBox_FontTest.ReadOnly = true;
+            this.textBox_FontTest.Size = new System.Drawing.Size(283, 21);
+            this.textBox_FontTest.TabIndex = 4;
+            this.textBox_FontTest.TabStop = false;
+            this.textBox_FontTest.Text = "C# 프로그래밍";
+            this.textBox_FontTest.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // trackBar_Dummy
+            // 
+            this.trackBar_Dummy.AutoSize = false;
+            this.trackBar_Dummy.LargeChange = 4;
+            this.trackBar_Dummy.Location = new System.Drawing.Point(12, 91);
+            this.trackBar_Dummy.Maximum = 20;
+            this.trackBar_Dummy.Name = "trackBar_Dummy";
+            this.trackBar_Dummy.Size = new System.Drawing.Size(282, 24);
+            this.trackBar_Dummy.TabIndex = 5;
+            this.trackBar_Dummy.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar_Dummy.Scroll += new System.EventHandler(this.tbDummy_Scroll);
+            // 
+            // progressBar_Dummy
+            // 
+            this.progressBar_Dummy.Location = new System.Drawing.Point(12, 121);
+            this.progressBar_Dummy.Maximum = 20;
+            this.progressBar_Dummy.Name = "progressBar_Dummy";
+            this.progressBar_Dummy.Size = new System.Drawing.Size(282, 13);
+            this.progressBar_Dummy.TabIndex = 6;
+            // 
+            // button_Modal
+            // 
+            this.button_Modal.Location = new System.Drawing.Point(12, 165);
+            this.button_Modal.Name = "button_Modal";
+            this.button_Modal.Size = new System.Drawing.Size(64, 23);
+            this.button_Modal.TabIndex = 7;
+            this.button_Modal.Text = "Modal";
+            this.button_Modal.UseVisualStyleBackColor = true;
+            this.button_Modal.Click += new System.EventHandler(this.onModal);
+            // 
+            // button_Modaless
+            // 
+            this.button_Modaless.Location = new System.Drawing.Point(97, 165);
+            this.button_Modaless.Name = "button_Modaless";
+            this.button_Modaless.Size = new System.Drawing.Size(78, 23);
+            this.button_Modaless.TabIndex = 8;
+            this.button_Modaless.Text = "Modaless";
+            this.button_Modaless.UseVisualStyleBackColor = true;
+            this.button_Modaless.Click += new System.EventHandler(this.onModaless);
+            // 
+            // button_MessageBox
+            // 
+            this.button_MessageBox.Location = new System.Drawing.Point(194, 165);
+            this.button_MessageBox.Name = "button_MessageBox";
+            this.button_MessageBox.Size = new System.Drawing.Size(101, 23);
+            this.button_MessageBox.TabIndex = 9;
+            this.button_MessageBox.Text = "MessageBox";
+            this.button_MessageBox.UseVisualStyleBackColor = true;
+            this.button_MessageBox.Click += new System.EventHandler(this.onMessageBox);
+            // 
+            // treeView_Dummy
+            // 
+            this.treeView_Dummy.Location = new System.Drawing.Point(12, 225);
+            this.treeView_Dummy.Name = "treeView_Dummy";
+            this.treeView_Dummy.Size = new System.Drawing.Size(282, 97);
+            this.treeView_Dummy.TabIndex = 10;
+            // 
+            // listView_Dummy
+            // 
+            this.listView_Dummy.Location = new System.Drawing.Point(12, 357);
+            this.listView_Dummy.Name = "listView_Dummy";
+            this.listView_Dummy.Size = new System.Drawing.Size(282, 157);
+            this.listView_Dummy.TabIndex = 11;
+            this.listView_Dummy.UseCompatibleStateImageBehavior = false;
+            // 
+            // button_AddRoot
+            // 
+            this.button_AddRoot.Location = new System.Drawing.Point(12, 328);
+            this.button_AddRoot.Name = "button_AddRoot";
+            this.button_AddRoot.Size = new System.Drawing.Size(75, 23);
+            this.button_AddRoot.TabIndex = 12;
+            this.button_AddRoot.Text = "루트 추가";
+            this.button_AddRoot.UseVisualStyleBackColor = true;
+            this.button_AddRoot.Click += new System.EventHandler(this.onAddRoot);
+            // 
+            // button_AddChild
+            // 
+            this.button_AddChild.Location = new System.Drawing.Point(220, 328);
+            this.button_AddChild.Name = "button_AddChild";
+            this.button_AddChild.Size = new System.Drawing.Size(75, 23);
+            this.button_AddChild.TabIndex = 13;
+            this.button_AddChild.Text = "자식 추가";
+            this.button_AddChild.UseVisualStyleBackColor = true;
+            this.button_AddChild.Click += new System.EventHandler(this.onAddChild);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 535);
+            this.Controls.Add(this.button_AddChild);
+            this.Controls.Add(this.button_AddRoot);
+            this.Controls.Add(this.listView_Dummy);
+            this.Controls.Add(this.treeView_Dummy);
+            this.Controls.Add(this.button_MessageBox);
+            this.Controls.Add(this.button_Modaless);
+            this.Controls.Add(this.button_Modal);
+            this.Controls.Add(this.progressBar_Dummy);
+            this.Controls.Add(this.trackBar_Dummy);
+            this.Controls.Add(this.textBox_FontTest);
+            this.Controls.Add(this.check_Italic);
+            this.Controls.Add(this.check_Bold);
+            this.Controls.Add(this.combo_Font);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "UI 컨트롤러 연습";
+            this.Load += new System.EventHandler(this.formLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Dummy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox combo_Font;
+        private System.Windows.Forms.CheckBox check_Bold;
+        private System.Windows.Forms.CheckBox check_Italic;
+        private System.Windows.Forms.TextBox textBox_FontTest;
+        private System.Windows.Forms.TrackBar trackBar_Dummy;
+        private System.Windows.Forms.ProgressBar progressBar_Dummy;
+        private System.Windows.Forms.Button button_Modal;
+        private System.Windows.Forms.Button button_Modaless;
+        private System.Windows.Forms.Button button_MessageBox;
+        private System.Windows.Forms.TreeView treeView_Dummy;
+        private System.Windows.Forms.ListView listView_Dummy;
+        private System.Windows.Forms.Button button_AddRoot;
+        private System.Windows.Forms.Button button_AddChild;
+    }
+}
+
---
+++ 20200610/ex01/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listView_Dummy.Columns.Add("Name",180,HorizontalAlignment.Left);
+            listView_Dummy.Columns.Add("Depth",81,HorizontalAlignment.Left);
+            listView_Dummy.View = View.Details;
+        }
+
+        // 폰트 변경 함수
+        private void changeFont()
+        {
+            if (combo_Font.SelectedIndex < 0)
+            {
+                return;
+            }
+
+            FontStyle style = FontStyle.Regular;
+
+            // 체크박스가 체크되었는지 확인
+            if (check_Bold.Checked)
+            {
+                style |= FontStyle.Bold;
+
+            }
+            if (check_Italic.Checked)
+            {
+                style |= FontStyle.Italic;
+            }
+
+            textBox_FontTest.Font = new Font((string)combo_Font.SelectedItem, 10, style);
+        }
+
+        // form이 실행 될 때
+        private void formLoad(object sender, EventArgs e)
+        {
+            // 폰트 정보 초기화
+            var Fonts = FontFamily.Families;
+            foreach (FontFamily item in Fonts)
+            {
+                // 콤보박스에 폰트들 저장
+                combo_Font.Items.Add(item.Name);
+            }
+        }
+
+        // 콤보박스 폰트 선택해서 클릭시 폰트 변경 함수 실행
+        private void onFontSelect(object sender, EventArgs e)
+        {
+            changeFont();
+        }
+
+        // 굵게 체크 시 폰트 변경 함수 실행
+        private void onBold(object sender, EventArgs e)
+        {
+            changeFont();
+        }
+
+        // 기울임 체크 시 폰트 변경 함수 실행
+        private void onItalic(object sender, EventArgs e)
+        {
+            changeFont();
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+
+        private void tbDummy_Scroll(object sender, EventArgs e)
+        {
+            progressBar_Dummy.Value = trackBar_Dummy.Value;
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+
+        private void onModal(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Text = "Modal Form";
+            form.Width = 300;
+            form.Height = 200;
+            form.BackColor = Color.Beige;
+            form.ShowDialog(); // Modal은 ShowDialog
+        }
+
+        private void onModaless(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Text = "Modaless Form";
+            form.Width = 300;
+            form.Height = 200;
+            form.BackColor = Color.Coral;
+            form.Show(); // Modaless는 Show
+        }
+
+        private void onMessageBox(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox_FontTest.Text, "메세지박스", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        private Random rand = new Random();
+
+        private void treeToList()
+        {
+            listView_Dummy.Items.Clear();
+            foreach (TreeNode item in treeView_Dummy.Nodes)
+            {
+                treeToList(item);
+            }
+        }
+
+        private void treeToList(TreeNode node)
+        {
+            listView_Dummy.Items.Add(new ListViewItem(new string[] { node.Text, node.FullPath.Count(f => f == '\\').ToString() }));
+            foreach (TreeNode item in node.Nodes)
+            {
+                treeToList(item);
+            }
+        }
+
+        private void onAddRoot(object sender, EventArgs e)
+        {
+            treeView_Dummy.Nodes.Add(rand.Next().ToString());
+            treeToList();
+        }
+
+        private void onAddChild(object sender, EventArgs e)
+        {
+            if(treeView_Dummy.SelectedNode == null)
+            {
+                MessageBox.Show("선택된 노드가 없습니다", "Tree View", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            treeView_Dummy.SelectedNode.Nodes.Add(rand.Next().ToString());
+            treeView_Dummy.SelectedNode.Expand();
+            treeToList();
+        }
+    }
+}
