1c8b78baec8d80102b833ddafb0c3c6da4781e48
sonny6310 <juny0413@naver.com>
2020-06-29 15:39:22 +0900

    c# portfolio 7

---
+++ Project/20200629/Apartment/SizableCalendar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Apartment
+{
+    public class SizableCalendar : MonthCalendar
+    {
+        [DllImport("uxtheme.dll", ExactSpelling = true, CharSet = CharSet.Unicode)]
+        static extern int SetWindowTheme(IntPtr hwnd, string pszSubAppName, string pszSubIdList);
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            SetWindowTheme(Handle, string.Empty, string.Empty);
+            base.OnHandleCreated(e);
+        }
+    }
+}
