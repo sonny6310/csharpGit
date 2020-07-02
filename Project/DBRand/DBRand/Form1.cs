5e9eda93bdcdf3df9ff8badaaa03c0683a49c54d
sonny6310 <juny0413@naver.com>
2020-07-02 16:47:48 +0900

    input rand data into mssql

---
+++ Project/DBRand/DBRand/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBRand
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            
+            
+        }
+        private int[] ho = { 101, 102, 103, 201, 202, 203, 301, 302, 303, 401, 402, 403, 501, 502, 503, 601, 602, 603, 701, 702, 703, 801, 802, 803, 901, 902, 903, 1001, 1002, 1003, 1101, 1102, 1103, 1201, 1202, 1203, 1301, 1302, 1303, 1401, 1402, 1403, 1501, 1502, 1503, 1601, 1602, 1603, 1701, 1702, 1703, 1801, 1802, 1803, 1901, 1902, 1903, 2001, 2002, 2003 };
+        private string strconn = "Data Source=192.168.0.200;Initial Catalog=jh_20200611;Persist Security Info=True;User Id=sa;Password=8765432!";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection sqlcon = new SqlConnection(strconn);
+                sqlcon.Open();
+
+                for (int i = 0; i < ho.Length; i++)
+                {
+                    SqlCommand cmd = new SqlCommand("insert into resident (building, unit, head,member,contact,car1,car2,pets,baby,remark,reg_date) values (101,@unit,@name,@num,@phone,@car1,@car2,@pet,@baby,@remark,getdate())", sqlcon);
+                    cmd.Parameters.AddWithValue("@unit", ho[i]);
+                    cmd.Parameters.AddWithValue("@name", RandData.getName());
+                    cmd.Parameters.AddWithValue("@num", RandData.getMem_num());
+                    cmd.Parameters.AddWithValue("@phone", RandData.getphone());
+                    cmd.Parameters.AddWithValue("@car1", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@car2", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@pet", RandData.getPet());
+                    cmd.Parameters.AddWithValue("@baby", RandData.getbaby());
+                    cmd.Parameters.AddWithValue("@remark", "전입");
+                    cmd.Parameters.AddWithValue("@ho", ho[i]);
+                    cmd.ExecuteNonQuery();
+                }
+
+                sqlcon.Close();
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show("\t\t-- 오류메시지 --\n" + except.Message + Environment.NewLine + Environment.NewLine + "\t\t-- 오류내용 --\n" + except.StackTrace);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection sqlcon = new SqlConnection(strconn);
+                sqlcon.Open();
+
+                for (int i = 0; i < ho.Length; i++)
+                {
+                    SqlCommand cmd = new SqlCommand("insert into resident (building, unit, head,member,contact,car1,car2,pets,baby,remark,reg_date) values (102,@unit,@name,@num,@phone,@car1,@car2,@pet,@baby,@remark,getdate())", sqlcon);
+                    cmd.Parameters.AddWithValue("@unit", ho[i]);
+                    cmd.Parameters.AddWithValue("@name", RandData.getName());
+                    cmd.Parameters.AddWithValue("@num", RandData.getMem_num());
+                    cmd.Parameters.AddWithValue("@phone", RandData.getphone());
+                    cmd.Parameters.AddWithValue("@car1", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@car2", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@pet", RandData.getPet());
+                    cmd.Parameters.AddWithValue("@baby", RandData.getbaby());
+                    cmd.Parameters.AddWithValue("@remark", "전입");
+                    cmd.Parameters.AddWithValue("@ho", ho[i]);
+                    cmd.ExecuteNonQuery();
+                }
+
+                sqlcon.Close();
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show("\t\t-- 오류메시지 --\n" + except.Message + Environment.NewLine + Environment.NewLine + "\t\t-- 오류내용 --\n" + except.StackTrace);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection sqlcon = new SqlConnection(strconn);
+                sqlcon.Open();
+
+                for (int i = 0; i < ho.Length; i++)
+                {
+                    SqlCommand cmd = new SqlCommand("insert into resident (building, unit, head,member,contact,car1,car2,pets,baby,remark,reg_date) values (103,@unit,@name,@num,@phone,@car1,@car2,@pet,@baby,@remark,getdate())", sqlcon);
+                    cmd.Parameters.AddWithValue("@unit", ho[i]);
+                    cmd.Parameters.AddWithValue("@name", RandData.getName());
+                    cmd.Parameters.AddWithValue("@num", RandData.getMem_num());
+                    cmd.Parameters.AddWithValue("@phone", RandData.getphone());
+                    cmd.Parameters.AddWithValue("@car1", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@car2", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@pet", RandData.getPet());
+                    cmd.Parameters.AddWithValue("@baby", RandData.getbaby());
+                    cmd.Parameters.AddWithValue("@remark", "전입");
+                    cmd.Parameters.AddWithValue("@ho", ho[i]);
+                    cmd.ExecuteNonQuery();
+                }
+
+                sqlcon.Close();
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show("\t\t-- 오류메시지 --\n" + except.Message + Environment.NewLine + Environment.NewLine + "\t\t-- 오류내용 --\n" + except.StackTrace);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection sqlcon = new SqlConnection(strconn);
+                sqlcon.Open();
+
+                for (int i = 0; i < ho.Length; i++)
+                {
+                    SqlCommand cmd = new SqlCommand("insert into resident (building, unit, head,member,contact,car1,car2,pets,baby,remark,reg_date) values (104,@unit,@name,@num,@phone,@car1,@car2,@pet,@baby,@remark,getdate())", sqlcon);
+                    cmd.Parameters.AddWithValue("@unit", ho[i]);
+                    cmd.Parameters.AddWithValue("@name", RandData.getName());
+                    cmd.Parameters.AddWithValue("@num", RandData.getMem_num());
+                    cmd.Parameters.AddWithValue("@phone", RandData.getphone());
+                    cmd.Parameters.AddWithValue("@car1", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@car2", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@pet", RandData.getPet());
+                    cmd.Parameters.AddWithValue("@baby", RandData.getbaby());
+                    cmd.Parameters.AddWithValue("@remark", "전입");
+                    cmd.Parameters.AddWithValue("@ho", ho[i]);
+                    cmd.ExecuteNonQuery();
+                }
+
+                sqlcon.Close();
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show("\t\t-- 오류메시지 --\n" + except.Message + Environment.NewLine + Environment.NewLine + "\t\t-- 오류내용 --\n" + except.StackTrace);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection sqlcon = new SqlConnection(strconn);
+                sqlcon.Open();
+
+                for (int i = 0; i < ho.Length; i++)
+                {
+                    SqlCommand cmd = new SqlCommand("insert into resident (building, unit, head,member,contact,car1,car2,pets,baby,remark,reg_date) values (105,@unit,@name,@num,@phone,@car1,@car2,@pet,@baby,@remark,getdate())", sqlcon);
+                    cmd.Parameters.AddWithValue("@unit", ho[i]);
+                    cmd.Parameters.AddWithValue("@name", RandData.getName());
+                    cmd.Parameters.AddWithValue("@num", RandData.getMem_num());
+                    cmd.Parameters.AddWithValue("@phone", RandData.getphone());
+                    cmd.Parameters.AddWithValue("@car1", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@car2", RandData.getCar());
+                    cmd.Parameters.AddWithValue("@pet", RandData.getPet());
+                    cmd.Parameters.AddWithValue("@baby", RandData.getbaby());
+                    cmd.Parameters.AddWithValue("@remark", "전입");
+                    cmd.Parameters.AddWithValue("@ho", ho[i]);
+                    cmd.ExecuteNonQuery();
+                }
+
+                sqlcon.Close();
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show("\t\t-- 오류메시지 --\n" + except.Message + Environment.NewLine + Environment.NewLine + "\t\t-- 오류내용 --\n" + except.StackTrace);
+            }
+        }
+    }
+}
---
+++ Project/DBRand/DBRand/RandData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBRand
+{
+    class RandData
+    {
+        static Random rand = new Random();
+
+        static string[] first = { "김", "강", "견", "고", "공", "구", "권", "기", "나", "남", "노", "류", "문", "모", "박", "방", "배", "봉", "석", "손", "설", "송", "안", "오", "원", "유", "이", "임", "장", "전", "조", "천", "최", "팽", "하", "한", "허" };
+        static string[] name = { "길", "세", "나", "민", "연", "지", "하", "윤", "채", "은", "수", "예", "서", "준", "도", "시", "우", "주", "권", "원", "호", "현", "우", "재", "건", "선", "승", "후", "태", "찬", "나", "율", "다", "인", "아" };
+        static string[] alpha = { "가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하", "거", "너", "더", "러", "머", "버", "서", "어", "저", "처", "커", "터", "퍼", "허", "고", "노", "도", "로", "모", "보", "소", "오", "조", "초", "코", "토", "포", "호", "구", "누", "두", "루", "무", "부", "수", "우", "주", "추", "쿠", "투", "푸", "후", "기", "니", "디", "리", "미", "비", "시", "이", "지", "치", "키", "티", "피", "히" };
+
+
+        public static string getName()
+        {
+            string fullName = first[rand.Next(0, first.Length)] + name[rand.Next(0, name.Length)] + name[rand.Next(0, name.Length)];
+            return fullName;
+        }
+
+        public static int getMem_num()
+        {
+            int Mem = rand.Next(1, 8);
+            return Mem;
+        }
+
+        public static string getCar()
+        {
+            string car = rand.Next(1, 100) + alpha[rand.Next(0,name.Length)] + rand.Next(1000, 10000);
+            return car;
+        }
+
+        public static int getPet()
+        {
+            int pet = rand.Next(0, 2);
+            return pet;
+        }
+
+        public static int getbaby()
+        {
+            int baby = rand.Next(0, 2);
+            return baby;
+        }
+
+        public static string getphone()
+        {
+            string ph = "010-" + rand.Next(1000, 10000)+"-"+rand.Next(1000, 10000);
+            return ph;
+        }
+
+    }
+}
