7d2a4c77b287bf9d8fe9cd9f56ee99998e81ab92
sonny6310 <juny0413@naver.com>
2020-07-08 15:35:32 +0900

    .

---
+++ Project/delegate_event_example/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private delegate void Del(int a, int b);
+
+        class A
+        {
+            public void print(int num1, int num2)
+            {
+                int sum = num1 + num2;
+                Console.WriteLine(sum);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            A clA = new A();
+            Del del = clA.print;
+
+            Console.WriteLine("clA.print");
+            clA.print(3,5);
+
+            Console.WriteLine("del");
+            del(3,5);
+        }
+    }
+}
---
+++ Project/delegate_event_example/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public delegate void Del(string msg);
+        public static event Del delEvent;
+
+        public Form1()
+        {
+            InitializeComponent();
+            IsMdiContainer = true;  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.MdiParent = this;
+            form2.StartPosition = FormStartPosition.Manual;
+            form2.Location = new Point(40, 50);
+            form2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            delEvent(textBox1.Text);
+        }
+    }
+}
